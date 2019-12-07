e257b752c213a991fe674ac64bd85d8f54b27ce2
bhishp <bhishma_p@hotmail.com>
2019-12-07 02:56:47 +0000

    Utilise react-json-tree for folding state data Style state boxes

--- src/FormikPanel.tsx
+++ src/FormikPanel.tsx
@@ -1,8 +1,10 @@
 import React, { CSSProperties } from 'react';
-import { useAddonState, useChannel } from '@storybook/api';
 import { FormikState } from 'formik';
-import { ADDON_ID, EVT_ON_SUBMIT, EVT_RENDER, EVT_SUBMIT } from './shared';
+import { useAddonState, useChannel } from '@storybook/api';
 import { STORY_RENDERED } from '@storybook/core-events';
+import JSONTree from 'react-json-tree';
+
+import { ADDON_ID, EVT_ON_SUBMIT, EVT_RENDER, EVT_SUBMIT } from './shared';
 
 const trafficLightStyle: CSSProperties = {
   display: 'inline-block',
@@ -53,14 +55,55 @@ const style: { [key: string]: CSSProperties } = {
   header: {
     display: 'flex',
     flexDirection: 'row',
+    height: '32px',
+    alignItems: 'center',
+    boxShadow: 'rgba(0,0,0,.1) 0 -1px 0 0',
+},
+  submitButton: {
+    height: '100%',
   },
-  submitButton: {},
   columns: {
     display: 'flex',
+    height: '100%',
   },
   column: {
-    flex: '0 1 25%',
+    flex: '1 1 25%',
+    height: '100%',
+    boxShadow: 'rgba(0,0,0,.1) 0 0 8px 0 inset'
   },
+  columnHeading: {
+    borderBottom: '1px solid rgba(0,0,0,0.1)',
+  },
+};
+
+// because JSONTree styling is limited
+const classRoot = 'formik-';
+const stateContainerClass = `${classRoot}stateContainer`;
+const injectCss =
+  `.${stateContainerClass} > ul {
+    height: 100%;
+    margin: 0;
+  }`;
+
+const eightiesTheme = {
+  scheme: 'eighties',
+  author: 'chris kempson (http://chriskempson.com)',
+  base00: '#2d2d2d',
+  base01: '#393939',
+  base02: '#515151',
+  base03: '#747369',
+  base04: '#a09f93',
+  base05: '#d3d0c8',
+  base06: '#e8e6df',
+  base07: '#f2f0ec',
+  base08: '#f2777a',
+  base09: '#f99157',
+  base0A: '#ffcc66',
+  base0B: '#99cc99',
+  base0C: '#66cccc',
+  base0D: '#6699cc',
+  base0E: '#cc99cc',
+  base0F: '#d27b53'
 };
 
 type Values = any;
@@ -87,47 +130,48 @@ export const FormikPanel = () => {
     values,
     errors,
     touched,
-    status,
+    // status,
     isValidating,
     isSubmitting,
     submitCount,
   } = formikState;
 
   return (
-    <div style={style.container}>
-      <div style={style.header}>
-        <button style={style.submitButton} onClick={() => emit(EVT_SUBMIT)}>
-          Submit Form
-        </button>
-        <BooleanState name="isValidating" value={isValidating} />
-        <BooleanState name="isSubmitting" value={isSubmitting} />
-        <div style={{ padding: '4px 16px' }}>
-          submitCount
-          <span style={{ marginLeft: '4px' }}>{submitCount}</span>
+    <>
+      <style dangerouslySetInnerHTML={{__html: injectCss}} />
+      <div style={style.container}>
+        <div style={style.header}>
+          <button style={style.submitButton} onClick={() => emit(EVT_SUBMIT)}>
+            Submit Form
+          </button>
+          <BooleanState name="isValidating" value={isValidating} />
+          <BooleanState name="isSubmitting" value={isSubmitting} />
+          <div style={{ padding: '4px 16px' }}>
+            submitCount
+            <span style={{ marginLeft: '4px' }}>{submitCount}</span>
+          </div>
+        </div>
+        <div style={style.columns}>
+          <div className={stateContainerClass} style={style.column}>
+            <h1 style={style.columnHeading}>Values</h1>
+            <JSONTree data={values || {}} hideRoot />
+          </div>
+          <div className={stateContainerClass} style={style.column}>
+            <h1 style={style.columnHeading}>Touched</h1>
+            <JSONTree data={touched || {}} hideRoot theme={eightiesTheme} />
+          </div>
+          <div className={stateContainerClass} style={style.column}>
+            <h1 style={style.columnHeading}>Errors</h1>
+            <JSONTree data={errors || {}} hideRoot />
+          </div>
+          {/*
+          <div className={stateContainerClass} style={{...style.column}}>
+            <h1>Submissions</h1>
+            <JSONTree data={submittedValues} hideRoot />
+          </div>
+          */}
         </div>
       </div>
-      <div style={style.columns}>
-        <div style={style.column}>
-          <h1>Values</h1>
-          <pre>{JSON.stringify(values, null, 2)}</pre>
-        </div>
-        <div style={style.column}>
-          <h1>Errors</h1>
-          <pre>{JSON.stringify(errors, null, 2)}</pre>
-        </div>
-        <div style={style.column}>
-          <h1>Touched</h1>
-          <pre>{JSON.stringify(touched, null, 2)}</pre>
-        </div>
-        <div style={style.column}>
-          <h1>Status</h1>
-          <pre>{JSON.stringify(status, null, 2)}</pre>
-        </div>
-        <div style={style.column}>
-          <h1>Submissions</h1>
-          <pre>{JSON.stringify(submittedValues, null, 2)}</pre>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
