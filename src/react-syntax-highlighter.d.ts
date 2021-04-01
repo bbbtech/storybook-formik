4a362da97f57e1dc3cd359156f9da1900a328ebd
bhishp <bhishp@users.noreply.github.com>
2021-04-01 17:17:06 +0100

    refactor: Ran format

--- src/react-syntax-highlighter.d.ts
+++ src/react-syntax-highlighter.d.ts
@@ -1,29 +1,31 @@
 // workaround from: https://github.com/storybookjs/storybook/issues/11684#issuecomment-674731469
 
 declare module 'react-syntax-highlighter/dist/cjs/create-element' {
-  import React from 'react'
+  import React from 'react';
 
   export type SyntaxHighlightNode =
     | (TextNode & Partial<Record<keyof TagNode<any>, never>>)
-    | (TagNode<any> & Partial<Record<keyof TextNode, never>>)
+    | (TagNode<any> & Partial<Record<keyof TextNode, never>>);
 
   export interface TextNode {
-    type: 'text'
-    value: string
+    type: 'text';
+    value: string;
   }
 
   export interface TagNode<T extends React.ElementType> {
-    tagName: T
-    properties: React.ComponentProps<T>
+    tagName: T;
+    properties: React.ComponentProps<T>;
   }
 
   export interface CreateElementProps {
-    node: SyntaxHighlightNode
-    stylesheet: import('react-syntax-highlighter').SyntaxHighlighterProps['customStyle']
-    style?: React.CSSProperties
-    useInlineStyles?: boolean
-    key: string | number
+    node: SyntaxHighlightNode;
+    stylesheet: import('react-syntax-highlighter').SyntaxHighlighterProps['customStyle'];
+    style?: React.CSSProperties;
+    useInlineStyles?: boolean;
+    key: string | number;
   }
 
-  export default function createElement(props: CreateElementProps): string | JSX.Element
+  export default function createElement(
+    props: CreateElementProps
+  ): string | JSX.Element;
 }
