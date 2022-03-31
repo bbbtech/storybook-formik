8e174b4587d2a667257764c0dee41b1e5b689f9d
Ítalo Teixeira <italo.brtx@gmail.com>
2022-03-31 20:40:37 +0200

    feat: support cjs and esm

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,6 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '../dist/register',
-  ],
+    '../preset.js'
+  ]
 };
--- register.js
+++ register.js
@@ -1,1 +1,12 @@
-require('./dist/register');
+function config(entry = []) {
+  return [...entry, require.resolve('./dist/esm/preset/preview')];
+}
+
+function managerEntries(entry = []) {
+  return [...entry, require.resolve('./dist/esm/preset/manager')];
+}
+
+module.exports = {
+  managerEntries,
+  config
+};
