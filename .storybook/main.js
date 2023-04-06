fcbd4e785d97710f2ad3cd16f142ae56262a175b
bhishp <bhishp@users.noreply.github.com>
2023-04-06 14:25:16 +0100

    Upgrade dependencies and test usage in storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '../dist/esm/register',
-  ]
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '../dist/esm/register'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  }
 };
